545dc7245853c193a72bbf003df34072df53110b
gst17 <gst17@mails.tsinghua.edu.cn>
2019-10-09 00:24:59 +0800

    s

--- utils.py
+++ utils.py
@@ -257,7 +257,7 @@ def get_ensemble_diversity_values(sess, x, y, predictions, number_model, X_test=
   return ensemble_diversity_records #len(X_test) X 1
 
 def log_style_distence(feature_map,num_model):
-  
+  style_loss_sum = K.mean(feature_map)
   return FLAGS.log_det_lamda*style_loss_sum
 
 def CE_loss(y_true,y_pred,num_model=FLAGS.num_models):
