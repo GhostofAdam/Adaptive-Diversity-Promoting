9d5d3512a1330eb91e573c19f9776245ce69f2e4
gst17 <gst17@mails.tsinghua.edu.cn>
2019-10-11 10:03:56 +0800

    test

--- utils.py
+++ utils.py
@@ -263,8 +263,8 @@ def log_style_distence(feature_map,num_model):
   for i in range(num_model):
     bs, height, width, filters = map(lambda i:i.value,f_p[i].get_shape())
     size = height * width * filters
-    feats = tf.reshape(f_p[i], (bs, height * width, filters))
-    feats_T = tf.transpose(feats, perm=[0,2,1,3])
+    feats = tf.reshape(f_p[i], [bs, height * width, filters])
+    feats_T = tf.transpose(feats, perm=[0,2,1])
     grams = tf.matmul(feats_T, feats) / size
     f_p[i] = grams
   for i in range(num_model):
